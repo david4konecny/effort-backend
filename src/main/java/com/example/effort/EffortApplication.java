1bbc7910ab6ef4b68818799b737976eb257c66fa
David Konecny <david4konecny@gmail.com>
2020-05-21 10:49:26 +0200

    initial commit

---
+++ src/main/java/com/example/effort/EffortApplication.java
@@ -0,0 +1,13 @@
+package com.example.effort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EffortApplication.class, args);
+	}
+
+}
